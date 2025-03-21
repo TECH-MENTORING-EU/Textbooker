aa9513ff13f601ac21eef57386b74479c368a582
MrFranQx <63866825+MrFranQx@users.noreply.github.com>
2025-03-21 15:42:01 +0100

    Form fixes and improvements:
    * change layout
    * translate remaining strings
    * split username and email
    * remove unneeded classes

--- Booker/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Booker/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -71,13 +71,16 @@ namespace Booker.Areas.Identity.Pages.Account
         /// </summary>
         public class InputModel
         {
+            [Required]
+            [Display(Name = "Nazwa użytkownika")]
+            public string UserName { get; set; }
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
             [Required]
             [EmailAddress]
-            [Display(Name = "Email")]
+            [Display(Name = "E-mail")]
             public string Email { get; set; }
 
             /// <summary>
@@ -87,7 +90,7 @@ namespace Booker.Areas.Identity.Pages.Account
             [Required]
             [StringLength(100, ErrorMessage = "{0} musi mieć co najmniej {2}, a maksymalnie {1} znaków.", MinimumLength = 6)]
             [DataType(DataType.Password)]
-            [Display(Name = "Password")]
+            [Display(Name = "Hasło")]
             public string Password { get; set; }
 
             /// <summary>
@@ -95,7 +98,7 @@ namespace Booker.Areas.Identity.Pages.Account
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
             [DataType(DataType.Password)]
-            [Display(Name = "Confirm password")]
+            [Display(Name = "Potwierdź hasło")]
             [Compare("Password", ErrorMessage = "Hasła się nie zgadzają.")]
             public string ConfirmPassword { get; set; }
         }
@@ -117,7 +120,7 @@ namespace Booker.Areas.Identity.Pages.Account
 
                 user.School = "ŚlTZN";
 
-                await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
+                await _userStore.SetUserNameAsync(user, Input.UserName, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
@@ -134,7 +137,7 @@ namespace Booker.Areas.Identity.Pages.Account
                         values: new { area = "Identity", userId = userId, code = code, returnUrl = returnUrl },
                         protocol: Request.Scheme);
 
-                    await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
+                    await _emailSender.SendEmailAsync(Input.Email, "Potwierdź swój e-mail",
                         $"Proszę potwierdź swoje konto klikając w ten <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>link</a>.");
 
                     if (_userManager.Options.SignIn.RequireConfirmedAccount)
