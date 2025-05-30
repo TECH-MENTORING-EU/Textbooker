8118d31d3639771719c286fbe8f1c4cc25008cbe
Tomasz Osmanowski <t.osmanowski@gmail.com>
2025-05-30 20:29:22 +0200

    photo upload to Azure (#53)

--- Booker/Services/StartupUtilities.cs
+++ Booker/Services/StartupUtilities.cs
@@ -3,6 +3,7 @@ using System.Configuration;
 using Booker.Data;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Migrations;
+using Azure.Storage.Blobs;
 
 namespace Booker.Services
 {
@@ -10,6 +11,7 @@ namespace Booker.Services
     {
         public static IServiceCollection AddBookerServices(this IServiceCollection services, IConfiguration configuration)
         {
+            services.AddSingleton(x => new BlobServiceClient(configuration["AzureStorage:ConnectionString"]));
             services.Configure<SmtpSettings>(configuration.GetSection("SmtpSettings"));           
             services.AddTransient<SendMailSvc>();
             return services;
