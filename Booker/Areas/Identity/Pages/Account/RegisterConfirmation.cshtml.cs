be102a810682303ce183477967145e4cca48fc9f
MrFranQx <63866825+MrFranQx@users.noreply.github.com>
2025-06-14 22:52:29 +0200

    Move email sending back to register handler, make the confirmation page just a message
    This fixes the problem with sending email from GET request in the simplest possible way.

--- Booker/Areas/Identity/Pages/Account/RegisterConfirmation.cshtml.cs
+++ Booker/Areas/Identity/Pages/Account/RegisterConfirmation.cshtml.cs
@@ -19,60 +19,14 @@ namespace Booker.Areas.Identity.Pages.Account
     [AllowAnonymous]
     public class RegisterConfirmationModel : PageModel
     {
-        private readonly UserManager<User> _userManager;
-        private readonly IEmailSender _sender;
-
-        public RegisterConfirmationModel(UserManager<User> userManager, IEmailSender sender)
+        public RegisterConfirmationModel()
         {
-            _userManager = userManager;
-            _sender = sender;
+
         }
 
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
-        public string Email { get; set; }
-
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
-        public bool DisplayConfirmAccountLink { get; set; }
-
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
-        public string EmailConfirmationUrl { get; set; }
-
-        public async Task<IActionResult> OnGetAsync(string userName, string email, string returnUrl = null)
+        public void OnGet(string email, string returnUrl = null)
         {
-            if (userName == null)
-            {
-                return RedirectToPage("/Index");
-            }
-            returnUrl = returnUrl ?? Url.Content("~/");
-
-            var user = await _userManager.FindByNameAsync(userName);
-            if (user == null)
-            {
-                return NotFound($"Unable to load user: '{userName}'.");
-            }
-
-            var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-            code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-
-            var callbackUrl = Url.Page(
-                        "/Account/ConfirmEmail",
-                        pageHandler: null,
-                        values: new { area = "Identity", userId = user.Id, code = code, returnUrl = returnUrl },
-                        protocol: Request.Scheme);
-
-            await _sender.SendEmailAsync(email, "Potwierdź swój e-mail",
-                      $"Proszę potwierdź swoje konto klikając w ten <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>link</a>.");
-
-            return Page();
+            
         }
     }
 }
