61b841be353a4ff28040eedbd3122155194ae65e
MrFranQx <63866825+MrFranQx@users.noreply.github.com>
2025-06-18 15:33:17 +0200

    Make UrlHelper Handler extension more consistent with built-in methods

--- Booker/TagHelpers/UrlHandlerExtensions.cs
+++ Booker/TagHelpers/UrlHandlerExtensions.cs
@@ -1,23 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Faker;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Routing;
 
 namespace Booker.TagHelpers;
 
 public static class UrlHandlerExtensions
 {
-    public static string Handler(this IUrlHelper urlHelper, string handler, object? values = null)
+    public static string? Handler(this IUrlHelper helper, string handler, object? values = null)
     {
+        ArgumentNullException.ThrowIfNull(helper);
+
         // Convert the values object to a dictionary
         var routeValues = new RouteValueDictionary(values)
         {
             ["handler"] = handler // Add the handler to the dictionary
         };
 
-        var url = urlHelper.RouteUrl(new UrlRouteContext
-        {
-            Values = routeValues
-        });
-
-        return url ?? throw new ArgumentException("Invalid handler");
+        return helper.RouteUrl(values);
     }
 }
