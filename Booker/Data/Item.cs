8899a8333d3ccdb7d90d6bcde7a8735e19bd5041
MrFranQx <63866825+MrFranQx@users.noreply.github.com>
2025-09-04 18:36:15 +0200

    Add db migration for admin toggles

--- Booker/Data/Item.cs
+++ Booker/Data/Item.cs
@@ -12,11 +12,13 @@ namespace Booker.Data
         public required Book Book { get; set; }
         public int UserId { get; init; } // Set by the database, not by the user, but needed for seeding
         public required User User { get; set; }
-        [Precision(10,2)]
+        [Precision(10, 2)]
         public required decimal Price { get; set; }
         public required DateTime DateTime { get; set; }
         public required string Description { get; set; }
         public required string State { get; set; }
-        public required string Photo {  get; set; }
+        public required string Photo { get; set; }
+        public bool IsVisible { get; set; } = true;
+        public bool CanChangeVisibility { get; set; } = true;
     }
 }
