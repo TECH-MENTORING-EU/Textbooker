a1455c38299d7e49df3492a765e5646818fcdb8b
MrFranQx <63866825+MrFranQx@users.noreply.github.com>
2025-09-07 21:49:16 +0200

    remove broken Humanizer reference

--- Booker/Areas/Admin/Pages/Admins.cshtml.cs
+++ Booker/Areas/Admin/Pages/Admins.cshtml.cs
@@ -1,7 +1,6 @@
 using System.Threading.Tasks;
 using Booker.Data;
 using Booker.Services;
-using Humanizer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
